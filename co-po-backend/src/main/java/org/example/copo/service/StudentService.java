7236e2a98d550bf8e8b5e3fdfb1c139fb6ede7ce
SinhaWiz <sinhartx2070@gmail.com>
2026-04-25 20:08:42 +0600

    Add StudentService.java

---
+++ co-po-backend/src/main/java/org/example/copo/service/StudentService.java
@@ -0,0 +1,37 @@
+package org.example.copo.service;
+
+import lombok.RequiredArgsConstructor;
+import org.example.copo.entity.Student;
+import org.example.copo.repository.StudentRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class StudentService {
+
+    private final StudentRepository studentRepository;
+
+    public List<Student> getAllStudents() {
+        return studentRepository.findAll();
+    }
+
+    public Student createStudent(Student student) {
+        return studentRepository.save(student);
+    }
+
+    public Student updateStudent(String id, Student studentDetails) {
+        Student student = studentRepository.findById(id).orElseThrow(() -> new RuntimeException("Student not found"));
+        student.setBatch(studentDetails.getBatch());
+        student.setName(studentDetails.getName());
+        student.setEmail(studentDetails.getEmail());
+        student.setDepartment(studentDetails.getDepartment());
+        student.setProgramme(studentDetails.getProgramme());
+        return studentRepository.save(student);
+    }
+
+    public void deleteStudent(String id) {
+        studentRepository.deleteById(id);
+    }
+}
