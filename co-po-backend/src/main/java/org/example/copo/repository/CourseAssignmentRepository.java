6b3f65cecc56e09948c256d3793704f76d24807a
SinhaWiz <sinhartx2070@gmail.com>
2026-04-25 20:08:42 +0600

    Add CourseAssignmentRepository.java

---
+++ co-po-backend/src/main/java/org/example/copo/repository/CourseAssignmentRepository.java
@@ -0,0 +1,10 @@
+package org.example.copo.repository;
+
+import org.example.copo.entity.CourseAssignment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CourseAssignmentRepository extends JpaRepository<CourseAssignment, CourseAssignment.CourseAssignmentId> {
+    List<CourseAssignment> findByFacultyId(String facultyId);
+}
