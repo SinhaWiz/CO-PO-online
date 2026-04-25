64075fb000ef68fdddb0ef286a8e3738159d868e
SinhaWiz <sinhartx2070@gmail.com>
2026-04-25 20:08:42 +0600

    Add EnrollmentRepository.java

---
+++ co-po-backend/src/main/java/org/example/copo/repository/EnrollmentRepository.java
@@ -0,0 +1,11 @@
+package org.example.copo.repository;
+
+import org.example.copo.entity.Enrollment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface EnrollmentRepository extends JpaRepository<Enrollment, Enrollment.EnrollmentId> {
+    List<Enrollment> findByCourseIdAndProgramme(String courseId, String programme);
+    List<Enrollment> findByStudentId(String studentId);
+}
